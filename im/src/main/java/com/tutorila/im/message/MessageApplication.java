b4f6b8ad17fd2ac08d65185ddaba7d413fdf384a
林敏 <33677300@qq.com>
2019-02-15 17:34:00 +0800

    1.submit configuration file.

--- im/src/main/java/com/tutorila/im/message/MessageApplication.java
+++ im/src/main/java/com/tutorila/im/message/MessageApplication.java
@@ -3,14 +3,29 @@ package com.tutorila.im.message;
 import java.net.InetSocketAddress;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.tutorila.im.message.server.MessageServer;
 
+/**
+ * 
+ * @author PANZERS
+ *
+ */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MessageApplication implements CommandLineRunner {
+	
+	@Value("${message.server.url}")
+	private String url;
+	
+	@Value("${message.server.port}")
+	private Integer port;
 	
 	@Autowired
     private MessageServer messageServer;
@@ -23,7 +38,7 @@ public class MessageApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("server start...");
 		//start
-        messageServer.run(new InetSocketAddress("127.0.0.1", 8080));
+        messageServer.run(new InetSocketAddress(url, port));
 	}
 
 }
